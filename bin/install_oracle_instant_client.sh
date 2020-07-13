a5c6334ad7459f1b1125b56abbbab7da890c657f
Joanis Rouanet <joanis.ang3@gmail.com>
2020-07-13 20:16:28 +0200

    Updated SH file installer

--- bin/install_oracle_instant_client.sh
+++ bin/install_oracle_instant_client.sh
@@ -8,14 +8,14 @@ fi
 echo "Installing Oracle instant client v12.2.0.1.0..."
 # @see https://gist.github.com/mazedlx/8e7764dfe68f08168d3f66a60256a863
 
-DIR_PACKAGES="$( cd "$( dirname -- "$0" )" && pwd )"/../packages
+BIN_DIR="$( cd "$( dirname -- "$0" )" && pwd )"
 
 mkdir /opt/oracle
 sudo apt install unzip
-unzip $DIR_PACKAGES/instantclient_12_2.zip -d /opt/oracle
+unzip $BIN_DIR/instantclient_12_2.zip -d /opt/oracle
 ln -s /opt/oracle/instantclient_12_2/libclntsh.so.12.1 /opt/oracle/instantclient_12_2/libclntsh.so
 ln -s /opt/oracle/instantclient_12_2/libocci.so.12.1 /opt/oracle/instantclient_12_2/libocci.so
 echo /opt/oracle/instantclient_12_2 > /etc/ld.so.conf.d/oracle-instantclient.conf
 ldconfig
-cd $DIR_PACKAGES/..
+cd $BIN_DIR/..
 echo "Done."
